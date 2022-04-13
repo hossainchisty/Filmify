7ea828a487b7bf7e2fd0c0506d5a1e8ddd126802
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-04-13 17:45:11 +0600

    🎉 initial apps release

---
+++ awards_and_events/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class AwardsAndEventsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'awards_and_events'
+    verbose_name = 'Awards and Events'
+    
---
+++ tvshows/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class TvshowsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tvshows'
+    verbose_name = 'TV Shows'
