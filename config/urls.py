65d936fae68bb7c344494cea0c65a22610eff776
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-05-14 19:25:04 +0600

    ✨ Configure apps urls

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include('movies.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
